b4e63a7d44e25ddd0b3739b5677d14c6587c286b
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2016-02-28 21:38:41 +0200

    kind of working

--- curtains.ino
+++ curtains.ino
@@ -2,20 +2,19 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
+
 namespace MotorCtl {
   const int STEPS_PER_REVOLUTION = 200;
-  const int RUN_INTERVAL = 30; // steps per interruption check
+  const int STEPS = 10; // steps per interruption check
   const int SPEED = 60;
 
-  const int SPPED_PIN_1 = 6;
-  const int SPPED_PIN_2 = 3;
+  const int SPPED_PIN_1 = 9;
+  const int SPPED_PIN_2 = 8;
   //const int SPPED_PIN_1 = 9;
   //const int SPPED_PIN_2 = 10;
 
-  Stepper myStepper(STEPS_PER_REVOLUTION, 2, 5, 4, 7);
+  Stepper myStepper(STEPS_PER_REVOLUTION, 4, 5, 7, 6);
   //Stepper myStepper(STEPS_PER_REVOLUTION, 8, 11, 12, 13);
-
-  volatile bool interrupted = false;
 
   void setup() {
     myStepper.setSpeed(SPEED);
@@ -26,22 +25,12 @@ namespace MotorCtl {
     digitalWrite(SPPED_PIN_2, HIGH);
   }
 
-  void runForward() {
-    interrupted = false;
-    while (!interrupted) {
-      myStepper.step(RUN_INTERVAL);
-    }
+  void stepForward() {
+    myStepper.step(STEPS);
   }
 
-  void runBackward() {
-    interrupted = false;
-    while (!interrupted) {
-      myStepper.step(-RUN_INTERVAL);
-    }
-  }
-
-  void stop() {
-    interrupted = true;
+  void stepBackward() {
+    myStepper.step(-STEPS);
   }
 
 }
@@ -51,8 +40,8 @@ namespace MotorCtl {
 
 namespace Controller {
 
-  const int PIN_REED_CLOSED = A0;
-  const int PIN_REED_OPEN = A1;
+  const int PIN_REED_CLOSED = 3;
+  const int PIN_REED_OPEN = 2;
 
   enum class State {UNKNOWN, OPEN, CLOSED, IN_TRANSITION};
 
@@ -65,8 +54,9 @@ namespace Controller {
   }
 
   void onReedClosedInterruption() {
+    Serial.print("onReedClosedInterruption ");
+    Serial.println(movingDir);
     if (movingDir == -1 && isReedActivated(PIN_REED_CLOSED)) {
-      MotorCtl::stop();
       lastMovingDir = movingDir;
       movingDir = 0;
       currentState = State::CLOSED;
@@ -74,8 +64,9 @@ namespace Controller {
   }
 
   void onReedOpenInterruption() {
+    Serial.print("onReedOpenInterruption ");
+    Serial.println(movingDir);
     if (movingDir == 1 && isReedActivated(PIN_REED_OPEN)) {
-      MotorCtl::stop();
       lastMovingDir = movingDir;
       movingDir = 0;
       currentState = State::CLOSED;
@@ -88,7 +79,6 @@ namespace Controller {
       return false;
     }
 
-    MotorCtl::runBackward();
     lastMovingDir = movingDir;
     movingDir = -1;
     currentState = State::IN_TRANSITION;
@@ -101,7 +91,6 @@ namespace Controller {
       return false;
     }
 
-    MotorCtl::runForward();
     lastMovingDir = movingDir;
     movingDir = 1;
     currentState = State::IN_TRANSITION;
@@ -110,7 +99,6 @@ namespace Controller {
 
   void stopTransition() {
     if (currentState == State::IN_TRANSITION) {
-      MotorCtl::stop();
       lastMovingDir = movingDir;
       movingDir = 0;
       currentState = State::UNKNOWN;
@@ -140,23 +128,56 @@ namespace Controller {
   void setup() {
     MotorCtl::setup();    
 
-    pinMode(PIN_REED_OPEN, INPUT);
-    pinMode(PIN_REED_CLOSED, INPUT);
-    digitalWrite(PIN_REED_OPEN, HIGH);    // Activate internal pullup resistor
-    digitalWrite(PIN_REED_CLOSED, HIGH);  // Activate internal pullup resistor
+    pinMode(PIN_REED_OPEN, INPUT_PULLUP);
+    pinMode(PIN_REED_CLOSED, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(PIN_REED_OPEN), onReedOpenInterruption, CHANGE);
     attachInterrupt(digitalPinToInterrupt(PIN_REED_CLOSED), onReedClosedInterruption, CHANGE);    
+  }
+
+  void loop() {
+    int t = millis();
+    if (movingDir == -1) {
+      MotorCtl::stepBackward();
+      Serial.println(millis() - t);
+    } else if (movingDir == 1) {
+      MotorCtl::stepForward();
+      Serial.println(millis() - t);
+    }
   }
 
 }
 
 
-void setup() {
-  // put your setup code here, to run once:
+namespace Button {
 
+  const int PIN_BUTTON = 14;
+
+  void setup() {
+    pinMode(PIN_BUTTON, INPUT_PULLUP);
+  }
+
+  bool isPressed(){
+    return digitalRead(PIN_BUTTON) == LOW;
+  }
+  
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Controller::setup();
+  Button::setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//  Serial.println(millis());
 
+  if (Button::isPressed()) {
+    delay(500); // avoid incidental doubleclicks
+    Serial.println("Button pressed");
+    Controller::toggle();
+  }
+
+  Controller::loop();
+  
 }
