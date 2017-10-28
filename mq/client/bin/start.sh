39360df2a2ada0f9b367ba403b48f7e540de3e9e
Aleksandr Gornostal <al.gornostal@gmail.com>
2017-10-28 21:27:34 +0300

    minor changes

--- mq/client/bin/start.sh
+++ mq/client/bin/start.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 cd ..
 
-./bin/env.sh
+source ./bin/env.sh
 cmd="API_URL=$API_URL ACCESS_TOKEN=$ACCESS_TOKEN ARDUINO_URL=$ARDUINO_URL node client.js"
 
 nohup bash -c "($cmd > client.out) &> client_err.out" &
