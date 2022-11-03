a93a86e88af6bb4971600f82e280e32e6a06fda5
Aleksandr Gornostal <al.gornostal@gmail.com>
2022-11-03 23:14:16 +0200

    Run with Heroku

--- mq/server/server.js
+++ mq/server/server.js
@@ -1,129 +1,134 @@
-const express = require("express")
-const http = require("http")
-const url = require("url")
-const WebSocket = require("ws")
-const bodyParser = require("body-parser")
-const timeout = require("connect-timeout")
+const express = require("express");
+const http = require("http");
+const url = require("url");
+const WebSocket = require("ws");
+const bodyParser = require("body-parser");
+const timeout = require("connect-timeout");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(timeout("30s"))
-app.use(haltOnTimedout)
+const app = express();
+app.use(bodyParser.json());
+app.use(timeout("30s"));
+app.use(haltOnTimedout);
 
-const allowedCommands = ["open", "close", "toggle", "stop"]
-const accessToken = process.env.ACCESS_TOKEN
-let wsConnection, longPollingResponse, longPollingTimer
+const allowedCommands = ["open", "close", "toggle", "stop"];
+const accessToken = process.env.ACCESS_TOKEN;
+let wsConnection, longPollingResponse, longPollingTimer;
 
 if (!accessToken) {
-  throw new Error("ACCESS_TOKEN env var must be set")
+  throw new Error("ACCESS_TOKEN env var must be set");
 }
 
-app.get("/health-check", function(req, res) {
+app.get("/health-check", function (req, res) {
   if (!longPollingResponse) {
-    res.status(500).send({ error: "No LP connection" })
-    return
+    res.status(500).send({ error: "No LP connection" });
+    return;
   }
 
   try {
-    sendLongPollingCommand("ping")
+    sendLongPollingCommand("ping");
   } catch (e) {
-    res.status(500).send({ error: e.message })
-    return
+    res.status(500).send({ error: e.message });
+    return;
   }
 
-  res.send({ ok: true })
-})
+  res.send({ ok: true });
+});
 
-app.post("/command", function(req, res) {
-  const clientIp = req.connection.remoteAddress
+app.post("/command", function (req, res) {
+  const clientIp = req.connection.remoteAddress;
   if (!req.query.accessToken || req.query.accessToken !== accessToken) {
-    log(`accessToken is not set or incorrect. IP ${clientIp}`)
-    res.status(403).send({ error: "accessToken is not set or incorrect" })
-    return
+    log(`accessToken is not set or incorrect. IP ${clientIp}`);
+    res.status(403).send({ error: "accessToken is not set or incorrect" });
+    return;
   }
 
-  const { command } = req.body
+  const { command } = req.body;
   if (!allowedCommands.includes(command)) {
-    log(`Unknown or empty command. IP ${clientIp}`)
-    res.status(400).send({ error: "Unknown or empty command" })
-    return
+    log(`Unknown or empty command. IP ${clientIp}`);
+    res.status(400).send({ error: "Unknown or empty command" });
+    return;
   }
 
   if (wsConnection) {
     try {
-      wsConnection.send(JSON.stringify({ command }))
-      res.send({ accepted: true, sent: true })
-      log(`Command sent: ${command}`)
+      wsConnection.send(JSON.stringify({ command }));
+      res.send({ accepted: true, sent: true });
+      log(`Command sent: ${command}`);
     } catch (e) {
-      res.status(500).send({ error: e.message })
+      res.status(500).send({ error: e.message });
     }
-    return
+    return;
   }
 
   if (longPollingResponse) {
     try {
-      sendLongPollingCommand(command)
+      sendLongPollingCommand(command);
     } catch (e) {
-      res.status(500).send({ error: e.message })
-      return
+      res.status(500).send({ error: e.message });
+      return;
     }
-    res.send({ accepted: true, sent: true })
-    return
+    res.send({ accepted: true, sent: true });
+    return;
   }
 
-  res.send({ accepted: true, sent: false })
-})
+  res.send({ accepted: true, sent: false });
+});
 
 function sendLongPollingCommand(command) {
-  longPollingResponse.send({ command })
-  longPollingResponse = null
-  clearTimeout(longPollingTimer)
-  log(`Command sent: ${command}`)
+  longPollingResponse.send({ command });
+  longPollingResponse = null;
+  clearTimeout(longPollingTimer);
+  log(`Command sent: ${command}`);
 }
 
 app.get("/long-polling", (req, res) => {
-  const clientIp = req.connection.remoteAddress
+  const clientIp = req.connection.remoteAddress;
   if (!req.query.accessToken || req.query.accessToken !== accessToken) {
-    log(`accessToken is not set or incorrect. IP ${clientIp}`)
-    res.status(403).send({ error: `accessToken is not set or incorrect` })
-    return
+    log(`accessToken is not set or incorrect. IP ${clientIp}`);
+    res.status(403).send({ error: `accessToken is not set or incorrect` });
+    return;
   }
 
-  longPollingResponse = res
-  clearTimeout(longPollingTimer)
+  longPollingResponse = res;
+  clearTimeout(longPollingTimer);
   longPollingTimer = setTimeout(() => {
-    longPollingResponse.sendStatus(204)
-    longPollingResponse = null
-  }, 29e3)
-})
+    longPollingResponse.sendStatus(204);
+    longPollingResponse = null;
+  }, 29e3);
+});
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server, path: "/subscribe" })
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server, path: "/subscribe" });
 
 wss.on("connection", function connection(ws, req) {
-  const clientIp = req.connection.remoteAddress
-  const location = url.parse(req.url, true)
-  if (!location.query.accessToken || location.query.accessToken !== accessToken) {
-    log(`Incorrect access token from client ${clientIp}`)
-    ws.close()
-    return
+  const clientIp = req.connection.remoteAddress;
+  const location = url.parse(req.url, true);
+  if (
+    !location.query.accessToken ||
+    location.query.accessToken !== accessToken
+  ) {
+    log(`Incorrect access token from client ${clientIp}`);
+    ws.close();
+    return;
   }
 
-  log(`Connected client ${clientIp}`)
-  wsConnection = ws
+  log(`Connected client ${clientIp}`);
+  wsConnection = ws;
 
   ws.on("close", () => {
-    log("Clear WS connection")
-    wsConnection = null
-  })
-})
+    log("Clear WS connection");
+    wsConnection = null;
+  });
+});
 
-server.listen(8080, () => log(`Listening on ${server.address().port}`))
+server.listen(process.env.PORT || 8080, () =>
+  log(`Listening on ${server.address().port}`)
+);
 
 function log(...args) {
-  console.log([new Date().toISOString(), ...args].join(' '))
+  console.log([new Date().toISOString(), ...args].join(" "));
 }
 
 function haltOnTimedout(req, res, next) {
-  if (!req.timedout) next()
+  if (!req.timedout) next();
 }
